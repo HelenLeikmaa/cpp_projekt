05cf5a27c69913694e73af685d5375bf9603ba0e
Marko7172 <57323360+Marko7172@users.noreply.github.com>
2024-04-27 20:51:09 +0300

    Add files via upload

---
+++ CLI_ver1/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "mainwindow.h"
+#include "DecHeader.h"
+#include <QApplication>
+#include <iostream>
+#include <filesystem>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    //QApplication a(argc, argv);
+    //MainWindow w;
+    //w.show();
+    //return a.exec();
+
+    // ------ Marko Smirnov test algus ------
+
+
+    // Input file (questions) reading
+    string filename = "questions.txt";
+    string inputFileDirectory = "C:/Users/smirn/Desktop/Qt 3/Kysitlus_Tartu2024/" + filename; // kuidas muuta project et uuriks project kausta?
+    vector<Question> formQuestions = makeQuestionObjects(inputFileDirectory);
+
+
+
+
+    // CLI start
+    QCoreApplication a(argc, argv);
+    displayCliTerminal(formQuestions);
+
+
+    return 0;
+
+
+    // ------ Marko smirnov test koodi lõpp  ------
+
+
+}
---
+++ CLI_ver1/question.cpp
@@ -0,0 +1,58 @@
+﻿#include "question.h"
+#include "decHeader.h"
+#include "vector"
+#include "string"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+vector<Question> makeQuestionObjects(const string& filename){
+
+    ifstream inputFile(filename);
+
+    // Check if the file is open
+    if (!inputFile.is_open()) {
+        cerr << "Failed to open the file: " << filename << endl;
+    }
+
+    string line;
+
+    if (inputFile.peek() == ifstream::traits_type::eof()) {
+        cerr << "The file is empty/missing: " << filename << endl;
+        inputFile.close();
+    }
+
+    vector<Question> questionObjects;
+
+    while (getline(inputFile, line)) {
+        vector<string> proccessedLine = splitString(line, ';');
+        vector<string> proccessedCorrectAnswers = splitString(proccessedLine.at(2), ',');
+        vector<string> proccessedWrongAnswers = splitString(proccessedLine.at(3), ',');
+
+        questionObjects.push_back(Question(proccessedLine.at(0), proccessedLine.at(1), proccessedCorrectAnswers, proccessedWrongAnswers));
+    }
+
+    // Close the file
+    inputFile.close();
+    return questionObjects;
+}
+
+
+
+vector<string> splitString(string& str, char delimiter) {
+    vector<string> tokens;
+    istringstream iss(str);
+    string words;
+
+    while (getline(iss, words, delimiter)) {
+        if (words[0] == ' '){
+            tokens.push_back(words.substr(1));
+        }
+        else{
+            tokens.push_back(words);
+        }
+    }
+
+    return tokens;
+}
